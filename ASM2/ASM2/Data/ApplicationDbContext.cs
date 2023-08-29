87473caae76696f545f546c9eaaba8875063ddd7
B0nusduck <88030859+B0nusduck@users.noreply.github.com>
2023-08-30 00:47:38 +0700

    disable codes that are no loger used

--- ASM2/ASM2/Data/ApplicationDbContext.cs
+++ ASM2/ASM2/Data/ApplicationDbContext.cs
@@ -16,17 +16,15 @@ namespace ASM2.Data
         *note this verson of lazy loading require an extention
         extention download instruction link:https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.Proxies/
         */
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseLazyLoadingProxies(); // Enable lazy loading
-        }
+        }*/
         public DbSet<ASM2.Models.Category> Category { get; set; } = default!;
         public DbSet<ASM2.Models.Product> Product { get; set; } = default!;
         public DbSet<ASM2.Models.Role> Role { get; set; } = default!;
-
         public DbSet<ASM2.Models.Cart> Cart { get; set; } = default!;
-
         public DbSet<ASM2.Models.CartItem> CartItem { get; set; } = default!;
 	}
 }
